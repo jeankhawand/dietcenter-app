932c4f1d2bb899053d39ca68d7ae27501d365dd1
Jean Khawand <jeankhawand@users.noreply.github.com>
2019-12-24 20:11:56 +0200

    fixes issues regarding docker added deployment capability on pull request 1/2 and fixed some layouts

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,37 +1,44 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require("./bootstrap");
 import {store} from "./store/index.js";
 import routes from "./routes.js";
 import VueRouter from "vue-router";
 import vuetify from "./vuetify";
 import App from "./components/App.vue";
-
-var $ = require("jquery");
-window.jQuery = $;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automaticqqally register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import {abilitiesPlugin} from '@casl/vue';
+import ability from './ability';
 
 window.Vue = require("vue");
 Vue.use(VueRouter);
+Vue.use(abilitiesPlugin, ability);
+Vue.config.productionTip = false;
 
 
 const router = new VueRouter({
     routes,
     mode: "history"
+});
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // used meta given by the router to check user logged in from vuex getters
+        if (!store.getters.loggedIn) {
+            next({
+                name: "Login"
+            });
+        } else {
+            next();
+        }
+    } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+        // used meta given by the router to check user if it's authenticator
+        if (store.getters.loggedIn) {
+            next({
+                name: "Dashboard"
+            });
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 });
 const app = new Vue({
     vuetify,
--- resources/js/components/Layout.vue
+++ resources/js/components/Layout.vue
@@ -33,10 +33,11 @@
       color="green"
       dark
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="loggeIn"/>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" v-if="loggedIn"/>
       <v-toolbar-title @click="goBack()" >Diet Center</v-toolbar-title>
        <v-spacer></v-spacer>
         <v-text-field
+            class="hidden-md-and-down"
             v-model="search"
             clearable
             flat
@@ -45,18 +46,21 @@
             label="Search"
         ></v-text-field>
       <v-spacer></v-spacer>
-
-      <v-btn @click="goCart()" text class="ma-2" dark>
-        <span class="hidden-sm-and-down">Cart</span>
-        <v-badge>
+        <v-btn  class="d-lg-none d-md" icon>
+            <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn v-if="loggedIn" class="hidden-sm-and-down" icon @click="handleFullScreen()">
+            <v-icon>mdi-fullscreen</v-icon>
+        </v-btn>
+      <v-btn icon @click="goCart()" >
+        <v-badge color="red" overlap >
           <template v-slot:badge>{{ cartItems }}</template>
-          <v-icon dark right>fas fa-shopping-cart</v-icon>
+          <v-icon dark medium>mdi-cart</v-icon>
         </v-badge>
       </v-btn>
 
-      <v-btn v-if="!loggedIn" text @click="loginIn()" class="ma-2" dark>
-          <span class="hidden-sm-and-down">Login</span>
-        <v-icon dark right>fas fa-lock</v-icon>
+      <v-btn v-if="!loggedIn" icon @click="loginIn()" >
+        <v-icon dark medium>fas fa-lock</v-icon>
       </v-btn>
       <v-avatar v-if="loggedIn">
         <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
@@ -95,19 +99,45 @@
     },
     cartItems() {
         return this.$store.state.cart.length;
-    }
+    },
+
   },
   methods: {
+      handleFullScreen() {
+
+              let doc = window.document;
+              let docEl = doc.documentElement;
+
+              let requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+              let cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+              if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+                  requestFullScreen.call(docEl);
+              }
+              else {
+                  cancelFullScreen.call(doc);
+              }
+      },
     logout() {
+
       this.$router.push({ name: "Logout" });
     },
     loginIn() {
+        if(this.$router.history.current.path === '/login'){
+            return;
+        }
       this.$router.push({ name: "Login" });
     },
     goBack() {
+        if(this.$router.history.current.path === '/'){
+            return;
+        }
       this.$router.push({ name: "Home" });
     },
     goCart() {
+        if(this.$router.history.current.path === '/cart'){
+        return;
+        }
       this.$router.push({ name: "Cart" });
     }
   }
