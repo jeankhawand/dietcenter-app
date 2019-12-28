9f8e92705d19b9e081786e5466c0c8ec4b19941a
Sergio Wehbe <sergiowehbe@gmail.com>
2019-12-29 00:40:54 +0200

    Set 0 instead of NULL in Ingredients Added price to Add recipe fundtion

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -18,36 +18,32 @@ class CheckRole
     {
         if (!$request->user()) {
             return response()->json('Unauthorized');
-        }
-        else{
-//            dd($request->user()); debugging
-//            fetch all the user roles as array
+        } else {
+            //dd($request->user()); debugging
+            //fetch all the user roles as array
             $roles = $request->user()->roles()->get();
-//            dd($roles[1]->name);
-//            number of roles that matches
+            //dd($roles[1]->name);
+            //number of roles that matches
             $match = 0;
-//            array role length
+            //array role length
             $count1 = count($roles);
-//            array role-middleware assigned length
+            //array role-middleware assigned length
             $count2 = count($rolesAssigned);
-            for($i=0; $i < $count1; $i++)
-            {
-                for($j=0; $j < $count2; $j++)
-                {
-                    if($roles[$i]->name == $rolesAssigned[$j]) {
-//                        echo $roles[$i]->name . ' and ' .$rolesAssigned[$j]; debugging
-//                        checking for matches
+            for ($i = 0; $i < $count1; $i++) {
+                for ($j = 0; $j < $count2; $j++) {
+                    if ($roles[$i]->name == $rolesAssigned[$j]) {
+                        //echo $roles[$i]->name . ' and ' .$rolesAssigned[$j]; debugging
+                        //checking for matches
                         $match++;
                     }
-
                 }
-//                return $next($request);
+                //return $next($request);
             }
-            if ($match>0){
-//                redirected to that requested request
+            if ($match > 0) {
+                //redirected to that requested request
                 return $next($request);
             }
-//          return json message
+            //return json message
             return response()->json('Unauthorized');
         }
 
